5d4875e6d49561a33b764953144df3df29a17659
João Melo <jacbmelo@gmail.com>
2014-05-18 12:28:59 +0100

    Google Analytics directive
    Angular Directive to track angular views on Google Analytics.

---
+++ app/scripts/directives/analytics.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('app')
+	.directive('analytics', ['$rootScope', '$location',
+    	function ($rootScope, $location) {
+    	return {
+        	link: function (scope, elem, attrs, ctrl) {
+            	$rootScope.$on('$routeChangeSuccess', function(event, currRoute, prevRoute) {
+                	ga('set', 'page', $location.path());
+                	ga('send', 'pageview');
+            	});
+        	}
+    	}
+	}]);
