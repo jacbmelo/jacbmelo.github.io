f0a626c17734bb8de7caa14a861fd52f795186ce
João Melo <jacbmelo@gmail.com>
2016-10-18 17:37:36 +0100

    Update analytics.js

--- app/scripts/directives/analytics.js
+++ app/scripts/directives/analytics.js
@@ -1,14 +1,17 @@
 'use strict';
 
 angular.module('app')
-	.directive('analytics', ['$rootScope', '$location',
-    	function ($rootScope, $location) {
-    	return {
-        	link: function (scope, elem, attrs, ctrl) {
-            	$rootScope.$on('$viewContentLoaded', function(event, currRoute, prevRoute) {
-                	ga('set', 'page', $location.path());
-                	ga('send', 'pageview');
-            	});
-        	}
-    	}
-	}]);
+	.directive('analytics', [
+		'$rootScope', 
+		'$location', 
+		function ($rootScope, $location) {
+			return {
+				link: function (scope, elem, attrs, ctrl) {
+					$rootScope.$on('$viewContentLoaded', function(event, currRoute, prevRoute) {
+						ga('set', 'page', $location.path());
+						ga('send', 'pageview');
+					});
+				}
+			}
+		}
+	]);
